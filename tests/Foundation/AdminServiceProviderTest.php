3f1df081618649c0d29346534f3654feb0b0303c
codesinging <codesinging@gmail.com>
2021-02-07 10:41:44 +0800

    Add test file: AdminServiceProviderTest.php

---
+++ tests/Foundation/AdminServiceProviderTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace CodeSinging\PinAdmin\Tests\Foundation;
+
+use CodeSinging\PinAdmin\Foundation\Admin;
+use CodeSinging\PinAdmin\Foundation\AdminServiceProvider;
+use Orchestra\Testbench\TestCase;
+
+class AdminServiceProviderTest extends TestCase
+{
+    protected function getPackageProviders($app): array
+    {
+        return [AdminServiceProvider::class];
+    }
+
+    public function testRegisterBinding()
+    {
+        self::assertInstanceOf(Admin::class, app(Admin::NAME));
+    }
+}
