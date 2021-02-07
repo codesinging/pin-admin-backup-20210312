eff6198bc5f4b31a1d8d6319161dc31ccb20de72
codesinging <codesinging@gmail.com>
2021-02-07 10:50:19 +0800

    Add test methods for AdminTest.php

--- tests/Foundation/AdminTest.php
+++ tests/Foundation/AdminTest.php
@@ -27,4 +27,53 @@ class AdminTest extends TestCase
     {
         return [AdminServiceProvider::class];
     }
+
+    public function testVersion()
+    {
+        self::assertEquals(Admin::VERSION, $this->admin->version());
+    }
+
+    public function testBrand()
+    {
+        self::assertEquals(Admin::BRAND, $this->admin->brand());
+    }
+
+    public function testSlogan()
+    {
+        self::assertEquals(Admin::SLOGAN, $this->admin->slogan());
+    }
+
+    public function testName()
+    {
+        self::assertEquals(Admin::NAME, $this->admin->name());
+    }
+
+    public function testGuard()
+    {
+        self::assertEquals(Admin::GUARD, $this->admin->guard());
+    }
+
+    public function testDirectory()
+    {
+        self::assertEquals(Admin::DIRECTORY, $this->admin->directory());
+        self::assertEquals(Admin::DIRECTORY . DIRECTORY_SEPARATOR . 'Controllers', $this->admin->directory('Controllers'));
+    }
+
+    public function testPath()
+    {
+        self::assertEquals(app_path(Admin::DIRECTORY), $this->admin->path());
+        self::assertEquals(app_path(Admin::DIRECTORY) . DIRECTORY_SEPARATOR . 'Controllers', $this->admin->path('Controllers'));
+    }
+
+    public function testGetNamespace()
+    {
+        self::assertEquals($this->app->getNamespace() . Admin::DIRECTORY, $this->admin->getNamespace());
+        self::assertEquals($this->app->getNamespace() . Admin::DIRECTORY . '\\Controllers', $this->admin->getNamespace('Controllers'));
+    }
+
+    public function testPackagePath()
+    {
+        self::assertEquals(dirname(dirname(__DIR__)), $this->admin->packagePath());
+        self::assertEquals(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'src', $this->admin->packagePath('src'));
+    }
 }
