458c28881036bd1fee38b86ff1ea3ce3a889722d
codesinging <codesinging@gmail.com>
2021-02-07 10:35:11 +0800

    Add AdminServiceProvider.php

---
+++ src/Foundation/AdminServiceProvider.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace CodeSinging\PinAdmin\Foundation;
+
+use Illuminate\Routing\Router;
+use Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider
+{
+    /**
+     * The console commands of PinAdmin.
+     * @var array
+     */
+    protected $commands = [];
+
+    /**
+     * The middlewares of PinAdmin.
+     * @var array
+     */
+    protected $middlewares = [
+    ];
+
+    /**
+     * Register PinAdmin services.
+     */
+    public function register()
+    {
+        $this->registerBinding();
+        $this->registerCommands();
+        $this->registerMiddleware();
+    }
+
+    /**
+     * Bootstrap PinAdmin services.
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the PinAdmin's binding to the container.
+     */
+    protected function registerBinding(): void
+    {
+        $this->app->singleton(Admin::NAME, Admin::class);
+    }
+
+    /**
+     * Register PinAdmin's console commands.
+     */
+    protected function registerCommands(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands($this->commands);
+        }
+    }
+
+    /**
+     * Register middleware for the application routes.
+     */
+    protected function registerMiddleware(): void
+    {
+        /** @var Router $router */
+        $router = $this->app['router'];
+
+        foreach ($this->middlewares as $key => $middleware) {
+            $router->aliasMiddleware($key, $middleware);
+        }
+    }
+}
